f1b359e7efbba54ca450bd5c25d3e144fd0d69fe
Edgar <some1just@gmail.com>
2020-07-17 12:34:06 -0600

    fixed

--- java.js
+++ java.js
@@ -1,21 +1,54 @@
+let coin = {
+    state: 0,
+    flip: function() {
+        /* 1. Randomly set your coin object's "state" property to be either 
+           0 or 1: use "this.state" to access the "state" property on this object.*/
 
- function myFunction() {
-       function getRandomInt (min, max) {
-            return Math.floor(Math.random() * (max - min) ) + min;
-        }
-        var num = getRandomInt(1, 3);
+         let d =  Math.random()
 
-        switch(num) {
-            case 1: 
-                answer = 'heads';
-                break;
-            case 2:
-                answer = 'tails';
-                break;
-        }
+         if (d < .5){
+            this.state = 1
+
+         }
+         else {
+            this.state = 0
+            } 
+    },
+    toString: function() {
+        /* 2. Return the string "Heads" or "Tails", depending on whether
+           "this.state" is 0 or 1. */
+
+           if (this.state === 1){
+                return "HEADS"
+           }
+           else {
+               return "TAILS"
+           }
         
-        document.getElementById('coin').innerHTML= answer;
+    },
+    toHTML: function() {
+        let image = document.createElement('img');
+        /* 3. Set the properties of this image element to show either face-up
+           or face-down, depending on whether this.state is 0 or 1.*/
+        return image;
+    }
 
-        }
-    
- 
+   
+};
+
+
+
+
+coin.flip()
+console.log(coin.toString())
+
+let dom = document.querySelector('main')
+dom.append(coin.toString())
+
+
+function display20Flips(){
+    for (let index = 0; index <= 20; index++) {
+        
+  console.log( coin.toString()[index])
+    } 
+}
